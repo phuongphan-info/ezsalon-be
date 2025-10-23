94b4df6cb3628951e2737862027a92f79c846de1
Phuong Phan <>
2025-10-23 18:45:56 +0700

    Delete unnecessary files

--- mysql-init/01-init.sql
+++
@@ -1,8 +0,0 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS ezsalon;
-
--- Use the database
-USE ezsalon;
-
--- Create sample data (optional)
--- This will be run when the container starts for the first time
